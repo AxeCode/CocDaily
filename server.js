113df19160d495755759d7b54cc46248212c9ff4
Yang <yangjinbao89757@gmail.com>
2016-01-25 13:55:54 +0800

    add new index and some static assets

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var http = require("http");
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.end('Hey, there...\n');
+var fs = require('fs');
+http.createServer(function(request, response) {
+    fs.readFile('index.html', function(err, data) {
+        if (err) {
+            response.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+        } else {
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            response.write(data.toString());
+        }
+        response.end();
+    });
 }).listen(80);
-
-console.log('Server running at http:127.0.0.1:80');
+console.log('Server running at http://127.0.0.1:80');
