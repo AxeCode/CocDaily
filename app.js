c144ce4a0f02f19e20e5fe8f9e10d074d93a263c
Young <yangjinbao89757@gmail.com>
2016-01-29 19:49:58 +0800

    a static server with express

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static('assets'));
+app.get('/', function(req, res) {
+    res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
